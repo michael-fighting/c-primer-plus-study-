82a91caaf1e6305bd865eb29a225fd3700dd55f2
michael-fighting <ding_meng@126.com>
2024-11-20 17:56:45 +0800

    11/19 study

---
+++ C++ Primer Plus第6版中文版源代码/Chapter 3/exercise/6.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char c = 88;
+    cout << c <<endl;
+    cout.put(char(88)) <<endl;
+    cout << char(88) << endl;
+
+    cout << (char)88 << endl;
+    double x1 = 2.0;
+    double x2 = 1.0;
+    double x3;
+    int x;
+    x = x1+x2;
+    x3 = x1+x2;
+    cout << x << endl;
+    cout << x3 << endl;
+
+}
