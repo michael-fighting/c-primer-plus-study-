a163775f98884a9722b547b9b02656cd40a5ecc4
michael-fighting <ding_meng@126.com>
2024-12-05 10:20:01 +0800

    12/5 study

---
+++ C++ Primer Plus第6版中文版源代码/Chapter 4/review.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+#include<array>
+
+using namespace std;
+
+int main(){
+    char actor[30];
+    short betsie[100];
+    float chuck[13];
+    long double dipsea[64];
+    
+    array <char, 30> actor;
+    array<short, 100> betsie;
+    array<float, 13> chuck;
+    array<long double, 64> dipsea;
+}
