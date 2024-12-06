cf6fab5438fdffed42e584d42ce416babc0d6437
michael-fighting <ding_meng@126.com>
2024-12-06 11:28:00 +0800

    12/5 study

--- C++ Primer Plus第6版中文版源代码/Chapter 4/review.cpp
+++ C++ Primer Plus第6版中文版源代码/Chapter 4/review.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
-
+#include<iostream>
 #include<array>
+#include<vector>
 
 using namespace std;
 
@@ -14,4 +15,42 @@ int main(){
     array<short, 100> betsie;
     array<float, 13> chuck;
     array<long double, 64> dipsea;
+    int five[5] = {1,3,5,7,9};
+    int even = five[0] + five[4];
+    cout<< chuck[1]<< endl;
+    char cha[12] = {'c','h','e','e','s','e','b','u','r','g','e','r'};
+    char cha2[13] = "cheeseburger";
+    string str = "Worldrf Salad";
+
+    struct fish
+    {
+        char type[20];
+        int weight;
+        float changdu;
+    };
+
+    fish ini
+    {
+        "bianyu",
+        5,
+        2.6,
+    };
+    enum Response {
+        No,
+        Yes,s
+        Maybe,
+    };
+    double ted;
+    double *zhizhen = &ted;
+    cout<<*zhizhen<<endl;
+    float treacle[10];
+    float *pt = treacle;
+    cout<< *pt<<endl;
+    cout << *(pt+9)<<endl;
+
+    unsigned int size;
+    cout << "Enter a positive interger: ";
+    cin >> size;
+    int * dyn = new int[input];
+    vector<int> dv(size);
 }
