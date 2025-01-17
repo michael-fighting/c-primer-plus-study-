c72a169b35a521482f300c1dcee555952e4a8854
michael-fighting <ding_meng@126.com>
2025-01-17 10:54:12 +0800

    12/28 29 study

---
+++ C++ Primer Plus第6版中文版源代码/Chapter 5/exercise/8.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<cstring>
+
+using namespace std
+
+int main{
+    char words[50];
+    int num;
+    cin.get(words, 50);
+    for (num = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+}
---
+++ C++ Primer Plus第6版中文版源代码/Chapter 5/review/6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int main(){
+    int i;
+    for(i = 1; i <=64; i=i*2){
+        cout<<i<<endl;
+    }
+    return 0;
+
+}
