db49a17caee8bae8fb8bdaf6631de30d660052ef
Your Name <you@example.com>
2025-01-25 12:18:35 +0800

    1/10 study

---
+++ C++ Primer Plus第6版中文版源代码/Chapter 6/review/4.cpp
@@ -0,0 +1,27 @@
+weight >= 115 && weight < 125;
+ch = 1 || ch = Q;
+x % 2 == 0 && x != 26;
+x % 2 -- 0 && x / 26 != 0;
+(donation >= 1000 && donation <= 2000) || guest == 1;
+(ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') 
+
+ if (x < 0)
+ {
+    x = -x;
+ }
+ 
+ char x;
+ int a_grade;
+ switch (x)
+ {
+ case 'A':
+    /* code */
+    a_grade++;
+    break;
+case 'B':
+    b_grade++;
+    break;
+ 
+ default:
+    break;
+ }
