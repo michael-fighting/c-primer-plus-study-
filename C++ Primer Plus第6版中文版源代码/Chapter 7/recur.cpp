0a531def9c15d3efe0fde23ece0cf52c7476dfba
Your Name <you@example.com>
2025-03-03 05:18:44 +0800

    2/1 2/2 2/3 2/4 2/5 study

--- C++ Primer Plus第6版中文版源代码/Chapter 7/recur.cpp
+++ C++ Primer Plus第6版中文版源代码/Chapter 7/recur.cpp
@@ -12,8 +12,8 @@ int main()
 void countdown(int n)
 {
     using namespace std;
-    cout << "Counting down ... " << n << endl;
+    cout << "Counting down ... " << n << &n << endl;
     if (n > 0)
         countdown(n-1);     // function calls itself
-    cout << n << ": Kaboom!\n";
+    cout << n << &n << ": Kaboom!\n";
 }
---
+++ C++ Primer Plus第6版中文版源代码/Chapter 7/review/2.cpp
@@ -0,0 +1,18 @@
+void igor();
+
+float tofo(int n);
+
+double mpg(double miles, double distances);
+long summation(long harray[], int size);
+
+double doctor(const char * s);
+
+
+struct boss
+{
+    /* data */
+};
+
+void ofcourse(boss a);
+
+char * plot(boss *b);
